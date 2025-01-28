8d277b7a04333dd38648c3e17e0637a2aa1d7d0d
Thomas Massarutto <thomas.massarutto@gmail.com>
2025-01-28 16:12:18 +0100

    miglioramenti e correzioni

--- SistemiOperativiC/3_Pipe/esempio.c
+++ SistemiOperativiC/3_Pipe/esempio.c
@@ -5,10 +5,14 @@
 #include <string.h>
 #include <sys/wait.h>
 
+/* Questo programma simula un processo padre che comunica con
+*  un processo figlio tramite una pipe
+*/
+
 int main(int argc, char *argv[]) {
 
-    // la pipe è vista come unu file
-    int pipefd[2];
+    // la pipe è vista come un array di due puntatori
+    int pipefd[2]; // 0: lettura, 1: scrittura
     pid_t cpid;
     char buf;
 
@@ -32,27 +36,28 @@ int main(int argc, char *argv[]) {
     /*UTILIZZO DELLA PIPE*/
 
     if (cpid==0){// se è il figlio
-        close(pipefd[1]); // deve solo scrivere, chiude la pipe in scrittura
+        close(pipefd[1]); // deve solo leggere, chiude la pipe in scrittura
 
         printf("Il figlio: aspetta che il padre riempia la pipe\n");
         /*legge li file e lo scrive a schermo*/
         while(read(pipefd[0], &buf, 1)>0){// legge un bite alla volta dall'estremo della pipe
-                                          // finche la pipe non viene chiusa  
-            write(STDOUT_FILENO, &buf, 1); // stampa il bite
+                                          // finche la pipe non viene chiusa 
+            write(STDOUT_FILENO, &buf, 1); // stampa il bite letto a terminale...
         }
-        
-        //non ci dono più bite da leggere -> uscita
+        //la pipe e stata chiusa dal padre
+
         write(STDOUT_FILENO, "\n", 1);
         printf("Il figlio ha smesso di parlare\n");
         exit(EXIT_SUCCESS);
+
     }else{// se e' il parent
-        close(pipefd[0]); // chiude l'estremo di lettura
+        close(pipefd[0]); // chiude l'estremo di lettura, deve solo scrivere nella pipe
         printf("Il padre dorme...\n");
         sleep(5);
         printf("Il padre si sveglia\n");
         /*scrive sull'estremo della pipe qualcosa per il figlio*/
         printf("Il padre: scrive su pipe\n");
-        int argv1_length= strlen(argv[1]);
+        int argv1_length= strlen(argv[1]); // calcola quanto è lungo il messaggio per la write()...
         write(pipefd[1], argv[1], argv1_length);// scrive sull'estremo della pipe
 
         // chiude la pipe
@@ -60,6 +65,7 @@ int main(int argc, char *argv[]) {
         close(pipefd[1]); // chiude la pipe cosi il figlio smette di leggere
 
         // aspetta il figlio per chiuderlo
+        printf("Il padre attende il figlio\n");
         wait(NULL);
         exit(EXIT_SUCCESS);
     }
